5a0311a9179eb0d8391b5292ebad09d4b0321ab7
Evgenii Guzhikhin <evgenii.guzhikhin@altium.com>
2023-01-10 21:52:54 +0100

    Add sleep mode and co2 led

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
+#include <LowPower.h>
+
 #include "EinkDisplay.h"
 #include "SensorCO2.h"
 #include "SensorTemHum.h"
 
 #define PWD_PIN 4
 #define TEMP_PIN 5
+#define LED_CO2_PIN 6
 
 EinkDisplay display;
 SensorCO2 sensorCO2(PWD_PIN);
@@ -12,6 +15,7 @@ SensorTemHum sensorTemHum(TEMP_PIN);
 
 void setup()
 {
+  pinMode(LED_CO2_PIN, OUTPUT);
 }
 
 void loop()
@@ -23,5 +27,18 @@ void loop()
       floor(sensorTemHum.tempC),
       floor(sensorTemHum.humidity));
 
+  digitalWrite(LED_CO2_PIN, sensorCO2.isNormal ? LOW : HIGH);
+
   display.loop();
+
+  // Sleep on 8 seconds
+  LowPower.idle(
+      SLEEP_8S,
+      ADC_OFF,
+      TIMER2_OFF,
+      TIMER1_OFF,
+      TIMER0_OFF,
+      SPI_OFF,
+      USART0_OFF,
+      TWI_OFF);
 }
